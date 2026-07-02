aa6145f99791de012173beb04beff21b7c60bc59
Atul Sharma <atul_sharma@epam.com>
2026-07-02 18:57:29 +0530

    fix: suppress unused var lint error in generator page

--- app/generator/page.tsx
+++ app/generator/page.tsx
@@ -33,6 +33,7 @@ const EXAMPLES = [
 ]
 
 export default function GeneratorPage() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { gen, setGen, activeTask, setActiveTask, stopActiveTask, abortRef, providers } = useLab()
   const { form, result, loading, error } = gen
 
